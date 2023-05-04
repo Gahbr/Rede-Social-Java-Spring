77d67ab087e54aeac9bafaa5c97e4ccd4e2b34f4
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2023-05-04 00:12:51 -0300

    :zap: feat: jwt no swagger

---
+++ src/main/java/com/sysmap/parrot/services/security/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.sysmap.parrot.services.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        security = {
+                @SecurityRequirement(name = "RequestedBy"),
+                @SecurityRequirement(name = "JWT")
+        }
+)
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .name("Authorization")
+                                .in(SecurityScheme.In.HEADER)
+                                .description("Insira o token JWT com o Bearer. Por exemplo: `Bearer abcde12345 `."))
+                        .addSecuritySchemes("RequestedBy", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .name("RequestedBy")
+                                .in(SecurityScheme.In.HEADER)
+                                .description("Insira o userId")))
+                .info(new Info()
+                        .title("Rede social Full-stack Parrot")
+                        .description("Documentação SWAGGER do sistema de rede social da Sysmap.")
+                        .version("1.0.0"));
+    }
+}
